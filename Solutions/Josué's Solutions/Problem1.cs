44de82e647bc414ee81772f0091024e159889807
Josué <molina.wilfredo.j@gmail.com>
2013-05-31 12:28:06 -0500

    I added a new folder and two files.
    I added the folder Josué's Solutions/ with the files Problem1.cs and
    README.txt.

---
+++ Solutions/Josué's Solutions/Problem1.cs
@@ -0,0 +1,19 @@
+﻿// Project Euler
+// Problem 1
+
+using System;
+
+class Problem1
+{
+    static void Main()
+    {
+        int sum = 0;
+
+        for (int i = 1; i < 1000; i++)
+        {
+            sum += i % 3 == 0 || i % 5 == 0 ? i : 0;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
---
+++ Solutions/Josué's Solutions/README.txt
@@ -0,0 +1,3 @@
+This folder is NOT meant to be used for cheating, but for code comparison upon problem resolution. Thank you!
+
+I write my solutions in C#.

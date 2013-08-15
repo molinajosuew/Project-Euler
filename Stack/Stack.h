7566f6d68d00649ffa67f2f3e4ab6e73e6036947
Josué <molina.wilfredo.j@gmail.com>
2013-08-15 15:50:43 -0500

    Linked List Implementation of Stack
    You may be asked to implement a stack. This is a linked list version of
    one.

---
+++ Stack/Stack.h
@@ -0,0 +1,19 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include "Node.h"
+
+class Stack
+{
+private:
+	Node* Head;
+
+public:
+	Stack();
+	void Push(int);
+	void Pop();
+	int Top();
+	bool IsEmpty();
+};
+
+#endif

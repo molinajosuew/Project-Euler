0b6e54888c85b90837970aa53efac3b86c38642c
Josué <molina.wilfredo.j@gmail.com>
2013-08-21 11:09:27 -0500

    PE: 7 & 8

---
+++ Solutions/Josué's Solutions/Problem7.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Program
+{
+    static bool IsPrime(int number)
+    {
+        if (number == 2)
+            return true;
+        if (number < 2 || number % 2 == 0)
+            return false;
+        for (int i = 3; i < number; i += 2)
+            if (number % i == 0)
+                return false;
+        return true;
+    }
+
+    static void Main()
+    {
+        int counter = 0, number = 1;
+        while (counter != 10001)
+            if (IsPrime(++number))
+                counter++;
+        Console.WriteLine(number);
+    }
+}

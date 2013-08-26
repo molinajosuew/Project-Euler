c3cba56b98c4f0e3a0d47b241a83437f0f19ca6b
Josué <molina.wilfredo.j@gmail.com>
2013-08-26 11:08:44 -0500

    Bettered the C++ Sieve Method

--- Tools/pe.h
+++ Tools/pe.h
@@ -3,19 +3,49 @@
 
 namespace pe
 {
-    bool* sieve(int n)
+    int* sieve(int n, int *s = 0)
     {
-        bool* m = new bool[n + 1]();
+        bool *m = new bool[n + 1]();
+
         for (int i = 2; i <= n; i++)
+        {
             m[i] = true;
+        }
+
         for (int i = 2; i <= n; i++)
         {
             if (!m[i])
+            {
                 continue;
+            }
+
             for (int j = 2 * i; j <= n; j += i)
+            {
                 m[j] = false;
+            }
         }
-        return m;
+        
+        for (int i = 0; i < n + 1; i++)
+        {
+            if (m[i])
+            {
+                (*s)++;
+            }
+        }
+
+        int *p = new int[*s], d = 0;
+
+        for (int i = 0; i < n + 1; i++)
+        {
+            if (m[i])
+            {
+                p[d++] = i;
+            }
+        }
+
+        delete [] m;
+
+        return p;
     }
 
     bool is_prime(int n)
